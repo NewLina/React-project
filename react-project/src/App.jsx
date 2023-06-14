027b8dab56b970e5623588e52eb30e4fb15f9b1c
Polina <=>
2023-06-14 20:54:36 +0300

    styled-header

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -4,18 +4,28 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Search from './components/Search/Search';
 import CardSlider from './components/CardSlider/CardSlider';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 
 function App() {
   return (
-    <>
-    <Header></Header>
-    <main className='main'>
-      <WordList></WordList>
-      <CardSlider ></CardSlider>
-    </main>
-    <Footer></Footer>
-    </>
+    <Router>
+      <>
+      <Header></Header>
+        <main className='main'>
+        <Routes>
+          <Route path="/game" element={<CardSlider/>}/>;
+          <Route path="/" element={<WordList/>} />;
+        </Routes>
+        </main>
+      <Footer></Footer>
+      </>
+      </Router>
   );
 }
 
--- react-project/src/components/Header/Header.jsx
+++ react-project/src/components/Header/Header.jsx
@@ -1,9 +1,27 @@
 import './header.scss';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+  } from 'react-router-dom';
 
 function Header() {
     
     return (
-<header className='header'> Header
+<header className='header'>
+    
+    <div className='header__logo logo'>
+    <Link to='/'><img className='logo__img' src="/images/logo.jpg" alt="logo" /></Link>
+    </div>
+    
+    
+    <nav className='header__navigation navigation'>
+        <ul className='navigation__list'>
+            <li className='navigation__list__item item--home'><Link className='navigation-link navigation-link--home' to="/">Home</Link></li>
+            <li className='navigation__list__item item--cards'><Link className='navigation-link navigation-link--cards' to="/game">Cards</Link></li>
+        </ul>
+    </nav>
 </header>
     );
 }
