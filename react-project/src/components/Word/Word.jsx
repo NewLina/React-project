66f62043c75869372e59e9bdb90fd339fa7c095d
Polina <prybak497@gmail.com>
2023-06-28 20:07:15 +0300

    validation_done

--- react-project/src/components/Word/Word.jsx
+++ react-project/src/components/Word/Word.jsx
@@ -6,13 +6,32 @@ const Word = (props) => {
     const [isEdited, setIsEdited] = useState(false);
     const [data, setData] = useState({english, transcription, russian});
     const [isEmpty, setIsEmpty] = useState(false);
+    const [isDisabled, setDisabled] = useState(false);
+    const [isError, setError] = useState(false);
+    
     const onEditFinished=(e)=>{
         if (!e.currentTarget?.value) {
+            setDisabled(true);
             setIsEmpty(true);
             return
+        } 
+        else if (validate(e.currentTarget?.value) === false) {
+            setDisabled(true);
+        } else {
+            setData({...data, [e.currentTarget.name]: e.currentTarget?.value});
+            setIsEmpty(false);
+            setDisabled(false);
         }
-        setData({...data, english: e.currentTarget?.value});
-        setIsEmpty(false);
+    }
+
+    const validate = (str) => {
+        const reg = new RegExp(/^[^0-9]*$/i);
+        if (str.match(reg)) {
+            setError(false);
+            console.log(`Result: ${str}`);
+            return true;
+        } setError(true); 
+        return false;
     }
 
     const buttonOnChange = (e) => {
@@ -24,12 +43,14 @@ const Word = (props) => {
     }
     if (isEdited) return (
         <tr className='table__item table__item__edited'>
-            <td><input className={isEmpty ? 'empty' : ''} type="text" onBlur={onEditFinished} defaultValue={data.english}/></td>
-            <td><input type="text" onBlur={onEditFinished}  defaultValue={data.transcription}/></td>
-            <td><input type="text" onBlur={onEditFinished}  defaultValue={data.russian}/></td>
-            <td><button className='button-save' onClick={buttonOnChange}>Save</button></td>
+            <td><input className={isEmpty ? 'empty' : 'full'} type="text" onChange={onEditFinished} defaultValue={data.english} name='english'/></td>
+            <td><input className={isEmpty ? 'empty' : 'full'} type="text" onChange={onEditFinished}  defaultValue={data.transcription} name='transcription'/></td>
+            <td><input className={isEmpty ? 'empty' : 'full'} type="text" onChange={onEditFinished}  defaultValue={data.russian} name='russian'/></td>
+            <td><button disabled = {isDisabled} className={isDisabled? 'button-save disabled-btn':'button-save'} onClick={buttonOnChange}>Save</button></td>
             <td><button className='button-cancel' onClick={makeEdited}>Cancel</button></td>
+            {isError&& <td> <span className='error'>Only letters are available</span></td>}
         </tr>
+        
     )
     return (
         <tr className='table__item'>
